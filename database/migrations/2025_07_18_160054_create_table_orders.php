3495e66d5597c69638755f504c4ba7eeefe1fda5
SoOvey <tryscaremeifyoucan@gmail.com>
2025-07-18 20:06:09 +0300

    создал миграции

---
+++ database/migrations/2025_07_18_160054_create_table_orders.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id()->primary();
+            $table->string('customer');
+            $table->unsignedBigInteger('warehouse_id');
+            $table->enum('status', ['active', 'completed', 'canceled']);
+
+            $table->timestamp('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders ');
+    }
+};
