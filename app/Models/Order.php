85324efa3632fce4063be64a6312350feea064db
SoOvey <tryscaremeifyoucan@gmail.com>
2025-07-19 11:40:08 +0300

    рефактор моделей

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
-class Orders extends Model
+class Order extends Model
 {
     protected $casts = [
         'completed_at' => 'datetime'
@@ -15,7 +15,7 @@ class Orders extends Model
 
     public function items(): HasMany
     {
-        return $this->hasMany(OrderItems::class, 'order_id', 'id');
+        return $this->hasMany(OrderItem::class);
     }
 
     public function warehouse(): BelongsTo
